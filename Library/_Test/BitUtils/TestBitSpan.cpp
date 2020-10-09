a4b259ab45988e6630a652f01133eb57d72d6abc
MesSer1024 <messer_@hotmail.com>
2020-10-09 22:55:11 +0200

    test fail build

--- Library/_Test/BitUtils/TestBitSpan.cpp
+++ Library/_Test/BitUtils/TestBitSpan.cpp
@@ -468,7 +468,7 @@ TEST_F(BitSpanFixture, setBitGetBit)
 {
 	const u32 NumWords = 100;
 	const u32 NumBits = NumWords * NumBitsInWord - 17;
-	BitWordType buffer[NumWords] a; // test autobuild
+	BitWordType buffer[NumWords] ab; // test autobuild
 
 	BitSpan span(buffer, NumBits);
 	span.clearAll();
